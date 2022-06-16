c43ec316d5cf50d25a6d5a154dd3b2d09ac89895
Nurul Hasan <75856743+Nurulhasan1338@users.noreply.github.com>
2022-06-17 00:44:36 +0530

    d

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,15 +16,15 @@ const Navbar=()=>{
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-      <li className="nav-item"> <Link className="nav-link" aria-current="page" to="/dailyhunt/general">Home</Link></li>
-       <li className="nav-item"> <Link className="nav-link" aria-current="page" to="/dailyhunt/About" >About</Link></li>
+      <li className="nav-item"> <Link className="nav-link" aria-current="page" to="/News_app/general">Home</Link></li>
+       <li className="nav-item"> <Link className="nav-link" aria-current="page" to="/News_app/About" >About</Link></li>
        <div className="hiddn">
-       <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page"  to="/dailyhunt/business">Business</Link></li> 
-        <li className="nav-item"> <Link className="nav-link py-2 fs-6 ps-1 Side" aria-current="page" to="/dailyhunt/entertainment">Entertainment</Link></li>
-        <li className="nav-item"> <Link className="nav-link py-2 fs-6 ps-1 Side" aria-current="page" to="/dailyhunt/health">Health</Link></li>
-        <li className="nav-item"> <Link className="nav-link py-2 fs-6 ps-1 Side" aria-current="page" to="/dailyhunt/science">Science</Link></li>
-        <li className="nav-item"> <Link className="nav-link py-2 fs-6 ps-1 Side" aria-current="page" to="/dailyhunt/sports">Sports</Link></li>
-        <li className="nav-item"> <Link className="nav-link py-2 fs-6 ps-1 Side" aria-current="page" to="/dailyhunt/technology">Technology</Link></li>
+       <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page"  to="/News_app/business">Business</Link></li> 
+        <li className="nav-item"> <Link className="nav-link py-2 fs-6 ps-1 Side" aria-current="page" to="/News_app/entertainment">Entertainment</Link></li>
+        <li className="nav-item"> <Link className="nav-link py-2 fs-6 ps-1 Side" aria-current="page" to="/News_app/health">Health</Link></li>
+        <li className="nav-item"> <Link className="nav-link py-2 fs-6 ps-1 Side" aria-current="page" to="/News_app/science">Science</Link></li>
+        <li className="nav-item"> <Link className="nav-link py-2 fs-6 ps-1 Side" aria-current="page" to="/News_app/sports">Sports</Link></li>
+        <li className="nav-item"> <Link className="nav-link py-2 fs-6 ps-1 Side" aria-current="page" to="/News_app/technology">Technology</Link></li>
        </div>
       </ul>
      
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,12 +9,12 @@ export default function Sidebar() {
     <div>
       <h3 className='px-2'>Categories</h3>
 <ul className="list-group list-group-flush ">
-        <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page" to="/dailyhunt/business">Business</Link></li> 
-        <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page" to="/dailyhunt/entertainment">Entertainment</Link></li>
-        <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page" to="/dailyhunt/health">Health</Link></li>
-        <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page" to="/dailyhunt/science">Science</Link></li>
-        <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page" to="/dailyhunt/sports">Sports</Link></li>
-        <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page" to="/dailyhunt/technology">Technology</Link></li>
+        <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page" to="/News_app/business">Business</Link></li> 
+        <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page" to="/News_app/entertainment">Entertainment</Link></li>
+        <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page" to="/News_app/health">Health</Link></li>
+        <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page" to="/News_app/science">Science</Link></li>
+        <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page" to="/News_app/sports">Sports</Link></li>
+        <li className="nav-item"> <Link className="nav-link py-2 fs-5 ps-2 Side" aria-current="page" to="/News_app/technology">Technology</Link></li>
  
 </ul>
 
